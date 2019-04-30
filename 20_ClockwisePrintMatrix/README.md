e761f8f4a17ea6a870015a636ba33d6e686234ec
Julian_Andrison <1546450025@qq.com>
2019-04-30 11:33:57 +0800

    20_ClockwisePrintMatrix

---
+++ 20_ClockwisePrintMatrix/README.md
@@ -0,0 +1,16 @@
+题目：输入一个矩阵，按照从外向里以顺时针依次打印出每个数字，例如:<br>
+![image](https://github.com/julianbb/Algorithm/blob/master/20_ClockwisePrintMatrix/image/1.png)<br>
+
+<br>
+输出: 1&ensp;2&ensp;3&ensp;6&ensp;9&ensp;8&ensp;7&ensp;4&ensp;5&ensp;
+
+
+
+<br>
+解法: <br>
+
+1. 先打印最外层，再打印次外层，递归调用
+
+
+
+
--- 20_ClockwisePrintMatrix/main2.cpp
+++ 20_ClockwisePrintMatrix/main2.cpp
@@ -3,16 +3,76 @@ using namespace std;
 
 
 
-
-void PrintArray_Clockwise(int* array, int rows, int cols)
+//传递的数组是[100][100] ,flag是原点的标志位，0代表(0,0), 1:(1,1), 2:(2,2)
+void PrintArray_Clockwise(int* array, int rows, int cols, int flag=0)
 {
     if(array == nullptr || rows <= 0 || cols <=0 )
 	return;
 
+    int flag_ud = 0; // 左右移动的标志位，1:向左，0:代表不动，-1:代表向右
+    int flag_lr = 1; // 上下移动的标志位, 1:向下，0:代表不动，-1:代表向上
+
+    int i =0; //数组索引,行
+    int j =0;
+
+    int tmp = array[(i+flag)*100+(j+flag)];//第一个元素
+    cout << tmp << " ";
     
+    while(1)
+    {
+	if(flag_lr == 1) //向右
+	{
+	    j++;
+	    if(j==cols)
+	    {
+		if(rows == 1) //防止只有一行情况
+		    break;
+		j--;
+		flag_lr = 0;
+		flag_ud = 1;
+	    }
+	}
+	if(flag_ud == 1) //向下
+	{
+	    i++;
+	    if(i == rows)
+	    {
+		if(cols == 1)//防止一列的情况
+		    break;
+		i--;
+		flag_ud = 0;
+		flag_lr = -1;
+	    }
+	}
+	if(flag_lr == -1) //向左 
+	{
+	    j--;
+	    if(j == -1)
+	    {
+		j++;
+		flag_lr = 0;
+		flag_ud = -1;
+	    }
+	}
+	if(flag_ud == -1) //向上
+	{
+	    i--;
+	    if(i == -1)
+	    {
+		i++;
+		flag_ud = 0;
+		flag_lr = 1;
+	    }
+	}
 
-    PrintArray_Clockwise(array+i*100+j, )
-
+	if(i==0 && j==0) //回到原点
+	    break;
+	else
+	cout << array[(i+flag)*100+(j+flag)] << " ";
+    }
+    
+       flag++; //原点往右下角移动１
+       PrintArray_Clockwise(array, rows-2, cols-2, flag); //递归调用
 }
 
 
@@ -36,8 +96,6 @@ void Print2DArray(int* array, int rows, int cols)
 	}
 	    cout << endl;
     }
-
-    cout << endl;
 }
 
 
@@ -47,7 +105,10 @@ void Test1()
 {
     int array[100][100] = {1};
     Print2DArray(*array, 1, 1);
+    cout << endl;
     PrintArray_Clockwise(*array, 1, 1);
+    cout << endl;
+    cout << endl;
 }
 
 
@@ -65,8 +126,10 @@ void Test2()
 	}
     }
     Print2DArray(*array, 2, 2);
+    cout << endl;
     PrintArray_Clockwise(*array, 2, 2);
-
+    cout << endl;
+    cout << endl;
     
 }
 
@@ -85,8 +148,10 @@ void Test3()
     }
 
     Print2DArray(*array, 3, 3);
+    cout << endl;
     PrintArray_Clockwise(*array, 3, 3);
-    
+    cout << endl;
+    cout << endl;
 }
 
 
@@ -103,10 +168,81 @@ void Test4()
 	}
     }
     Print2DArray(*array, 4, 4);
+    cout << endl;
     PrintArray_Clockwise(*array, 4, 4);
+    cout << endl;
+    cout << endl;
 }
 
 
+// 4*3
+void Test5()
+{
+    int array[100][100]={0};
+    int num = 0;
+    for(int i=0; i<4; i++)
+    {
+	for(int j=0; j<3; j++)
+	{
+	    array[i][j] = ++num;
+	}
+    }
+    Print2DArray(*array, 4, 3);
+    cout << endl;
+    PrintArray_Clockwise(*array, 4, 3);
+    cout << endl;
+    cout << endl;
+}
+
+//1*10
+void Test6()
+{
+    int array[100][100]={0};
+    int num = 0;
+    for(int i=0; i<1; i++)
+    {
+	for(int j=0; j<10; j++)
+	{
+	    array[i][j] = ++num;
+	}
+    }
+    Print2DArray(*array, 1, 10);
+    cout << endl;
+    PrintArray_Clockwise(*array, 1, 10);
+    cout << endl;
+    cout << endl;
+}
+
+// 10*1
+void Test7()
+{
+    int array[100][100]={0};
+    int num = 0;
+    for(int i=0; i<10; i++)
+    {
+	for(int j=0; j<1; j++)
+	{
+	    array[i][j] = ++num;
+	}
+    }
+    Print2DArray(*array, 10, 1);
+    cout << endl;
+    PrintArray_Clockwise(*array, 10, 1);
+    cout << endl;
+    cout << endl;
+}
+
+// 0*0
+void Test8()
+{
+    int array[100][100]={0};
+    Print2DArray(*array, 0, 0);
+    cout << endl;
+    PrintArray_Clockwise(*array, 0, 0);
+    cout << endl;
+    cout << endl;
+
+}
 
 
 int main(void)
@@ -115,7 +251,10 @@ int main(void)
     Test2();
     Test3();
     Test4();
-
+    Test5();
+    Test6();
+    Test7();
+    Test8();
     return 0;
 }
 
