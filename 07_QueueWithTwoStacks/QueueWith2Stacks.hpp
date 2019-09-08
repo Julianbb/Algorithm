c64927598b71a531932fb7921123c1f495b89a86
julianbb <1546450025@qq.com>
2019-09-08 17:02:07 +0800

    second time

--- 07_QueueWithTwoStacks/QueueWith2Stacks.hpp
+++ 07_QueueWithTwoStacks/QueueWith2Stacks.hpp
@@ -40,36 +40,87 @@ CQueue<T>::~CQueue(void)
 }
 
 
-	
 template<typename T>
 void CQueue<T>::appendTail(const T& node)
 {
     stack1.push(node);
 }
 
-
-
 template<typename T>
 T CQueue<T>::deleteHead()
 {
-
-    if(stack2.empty())
+    if(stack2.size() > 0)
     {
-	if(stack1.empty())
-	{
-	    std::logic_error ex("queue is empty");
-	    throw new exception(ex);
-	}
-	while(stack1.size())
-	{
-	    T midValue = stack1.top();
-	    stack2.push(midValue);
-	    stack1.pop();
-	}
-	
-    }
 	T tmp = stack2.top();
 	stack2.pop();
 	return tmp;
+    }
+    else
+    {
+	while(stack1.size()>0)
+	{
+	    T tmp = stack1.top();
+	    stack2.push(tmp);
+	    stack1.pop();
+	}
 
+	T tmp = stack2.top();
+	stack2.pop();
+	return tmp;
+    }
 }
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// template<typename T>
+// void CQueue<T>::appendTail(const T& node)
+// {
+//     stack1.push(node);
+// }
+
+
+
+// template<typename T>
+// T CQueue<T>::deleteHead()
+// {
+
+//     if(stack2.empty())
+//     {
+//         if(stack1.empty())
+//         {
+//             std::logic_error ex("queue is empty");
+//             throw new exception(ex);
+//         }
+//         while(stack1.size())
+//         {
+//             T midValue = stack1.top();
+//             stack2.push(midValue);
+//             stack1.pop();
+//         }
+
+//     }
+//         T tmp = stack2.top();
+//         stack2.pop();
+//         return tmp;
+
+// }
