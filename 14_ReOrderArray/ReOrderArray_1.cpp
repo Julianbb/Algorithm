f86f148f543a2ef10cb57762aa21a3ff8d870ebb
julianbb <1546450025@qq.com>
2019-09-10 21:38:31 +0800

    second time

--- 14_ReOrderArray/ReOrderArray_1.cpp
+++ 14_ReOrderArray/ReOrderArray_1.cpp
@@ -1,32 +1,56 @@
 #include <iostream>
-using namespace std;
-
-
+using namespace std; 
 
 
 
 void ReOrderArray(int* array, int length)
 {
-    if(array == nullptr || length<=1) return;
+    if(array == nullptr || length <= 0)
+	return ;
 
-    int p1 = 0;
-    int p2 = length-1;
-
-    while(p1 < p2)
+    int front=0, end=length-1;
+    while(front < end)
     {
-	while(array[p1]%2 == 1)
-	    p1++;
-	while(array[p2]%2 == 0)
-	    p2--;
+	while((array[front] & 0x01) && front < end ) //odd number
+	    front++;
 
-	if(p1<p2)
+	while(((array[end] & 0x01) == 0) && front < end) // even number 
+	    end--;
+
+	if(front < end)
 	{
-	    int tmp = array[p1];
-	    array[p1] = array[p2];
-	    array[p2] = tmp;
+	    int tmp = array[front];
+	    array[front] = array[end];
+	    array[end] = tmp;
 	}
     }
 }
+
+
+
+
+// void ReOrderArray(int* array, int length)
+// {
+//     if(array == nullptr || length<=1) return;
+
+//     int p1 = 0;
+//     int p2 = length-1;
+
+//     while(p1 < p2)
+//     {
+//         while(array[p1]%2 == 1)
+//             p1++;
+//         while(array[p2]%2 == 0)
+//             p2--;
+
+//         if(p1<p2)
+//         {
+//             int tmp = array[p1];
+//             array[p1] = array[p2];
+//             array[p2] = tmp;
+//         }
+//     }
+// }
 
 
 void PrintArray(int* array, int length)
