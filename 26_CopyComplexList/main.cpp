8920bd9b57aba77894161adaa30d4599259005fb
julianbb <1546450025@qq.com>
2019-09-13 18:13:38 +0800

    second time

--- 26_CopyComplexList/main.cpp
+++ 26_CopyComplexList/main.cpp
@@ -1,78 +1,162 @@
 #include "ComplexList.h"
+#include <map>
 
-// 先复制next
-ComplexListNode* Clone_next(ComplexListNode* pHead)
+
+
+
+ComplexListNode* CloneNext(ComplexListNode* pHead , map<ComplexListNode*, ComplexListNode*>& hashtable)
 {
-	if(pHead == nullptr) return nullptr;
-	ComplexListNode* pNode = pHead;
-	ComplexListNode* pClone_nextHead = nullptr;
-	ComplexListNode* pClone_nextTail = nullptr;
-	int i=0;
-	while(pNode!=nullptr)
-	{
-		ComplexListNode* pTmp = CreateNode(pNode->m_Value);
-		if(i==0)
-		{
-			pClone_nextHead = pTmp;
-			pClone_nextTail = pTmp;
-			i++;
-		}
-		else
-		{
-			pClone_nextTail->m_pNext = pTmp;
-			pClone_nextTail = pTmp;
-		}
-		pNode = pNode->m_pNext;
-	}
-	return pClone_nextHead;
+    if(pHead == nullptr) return nullptr;
+
+    ComplexListNode* cnode = pHead;
+    ComplexListNode* tmp_head;
+
+    while(cnode != nullptr)
+    {
+	int value = cnode->m_Value;
+	ComplexListNode* tmp = CreateNode(value);
+
+	if(hashtable.size() == 0)// 第一个节点
+	    tmp_head = tmp;
+
+	BuildNodes(tmp, cnode->m_pNext, nullptr); 
+
+	hashtable.insert(make_pair(cnode, tmp));
+
+	cnode = cnode->m_pNext;
+    }
+
+    return tmp_head;
 }
 
 
-//找数字，返回ComplexListNode*
-ComplexListNode* FindNumber(ComplexListNode* pHead, int target)
+
+void CloneSibling(ComplexListNode* pHead, ComplexListNode* pResult, map<ComplexListNode*, ComplexListNode*>& hashtable)
 {
-	if(pHead == nullptr)return nullptr;
-	ComplexListNode* pNode = pHead;
-	while(pNode != nullptr)
+    if(pHead == nullptr || hashtable.size() == 0) return;
+
+    ComplexListNode* cnode = pHead;
+    ComplexListNode* cresult = pResult;
+
+    while(cnode != nullptr && cresult != nullptr)
+    {
+	ComplexListNode* sibling = cnode->m_pSibling;
+	map<ComplexListNode*, ComplexListNode*>::iterator it = hashtable.find(sibling);
+	if(it == hashtable.end())
 	{
-		if(pNode->m_Value == target)
-			break;
-		pNode = pNode->m_pNext;
+	    pResult = nullptr;
+	    return;
 	}
-	return pNode;
-}
+	else
+	{
+	    cresult->m_pSibling = (*it).second;
+	}
 
 
-//再复制sibling
-void  Clone_Sibling(ComplexListNode* pClone, ComplexListNode* pHead)
-{
-	if(pHead == nullptr || clone == nullptr) return;
-	ComplexListNode* pNode_b = pHead;
-	ComplexListNode* pNode_c = pClone;
-	while(pNode_b!=nullptr||pNode_c!=nullptr)
-	{
-		if(pNode_b->m_pSibling != nullptr)
-		{
-			int num = pNode_b->m_pSibling->m_Value;
-			ComplexListNode* pTargetNum = FindNumber(pClone, num);	
-			pNode_c->m_pSibling = pTargetNum;
-		}
-		pNode_b = pNode_b->m_pNext;
-		pNode_c = pNode_c->m_pNext;
-	}
+	cnode = cnode->m_pNext;
+	cresult = cresult->m_pNext;
+    }
+
 }
 
 
 
 ComplexListNode* Clone(ComplexListNode* pHead)
 {
-	if(pHead == nullptr) return nullptr;
-	ComplexListNode* pNode = nullptr;
-		
-	pNode = Clone_next(pHead);
-	 		Clone_Sibling(pNode, pHead);
-	return pNode;
+    if(pHead == nullptr) return nullptr;
+
+    map<ComplexListNode*, ComplexListNode*> table;
+    ComplexListNode* result = CloneNext(pHead, table);
+    CloneSibling(pHead, result, table);
+
+
+    return result;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// 先复制next
+// ComplexListNode* Clone_next(ComplexListNode* pHead)
+// {
+//         if(pHead == nullptr) return nullptr;
+//         ComplexListNode* pNode = pHead;
+//         ComplexListNode* pClone_nextHead = nullptr;
+//         ComplexListNode* pClone_nextTail = nullptr;
+//         int i=0;
+//         while(pNode!=nullptr)
+//         {
+//                 ComplexListNode* pTmp = CreateNode(pNode->m_Value);
+//                 if(i==0)
+//                 {
+//                         pClone_nextHead = pTmp;
+//                         pClone_nextTail = pTmp;
+//                         i++;
+//                 }
+//                 else
+//                 {
+//                         pClone_nextTail->m_pNext = pTmp;
+//                         pClone_nextTail = pTmp;
+//                 }
+//                 pNode = pNode->m_pNext;
+//         }
+//         return pClone_nextHead;
+// }
+
+
+//找数字，返回ComplexListNode*
+// ComplexListNode* FindNumber(ComplexListNode* pHead, int target)
+// {
+//         if(pHead == nullptr)return nullptr;
+//         ComplexListNode* pNode = pHead;
+//         while(pNode != nullptr)
+//         {
+//                 if(pNode->m_Value == target)
+//                         break;
+//                 pNode = pNode->m_pNext;
+//         }
+//         return pNode;
+// }
+
+
+//再复制sibling
+// void  Clone_Sibling(ComplexListNode* pClone, ComplexListNode* pHead)
+// {
+//         if(pHead == nullptr || clone == nullptr) return;
+//         ComplexListNode* pNode_b = pHead;
+//         ComplexListNode* pNode_c = pClone;
+//         while(pNode_b!=nullptr||pNode_c!=nullptr)
+//         {
+//                 if(pNode_b->m_pSibling != nullptr)
+//                 {
+//                         int num = pNode_b->m_pSibling->m_Value;
+//                         ComplexListNode* pTargetNum = FindNumber(pClone, num);
+//                         pNode_c->m_pSibling = pTargetNum;
+//                 }
+//                 pNode_b = pNode_b->m_pNext;
+//                 pNode_c = pNode_c->m_pNext;
+//         }
+// }
+
+
+
+// ComplexListNode* Clone(ComplexListNode* pHead)
+// {
+//         if(pHead == nullptr) return nullptr;
+//         ComplexListNode* pNode = nullptr;
+
+//         pNode = Clone_next(pHead);
+//                          Clone_Sibling(pNode, pHead);
+//         return pNode;
+// }
 
 
 
