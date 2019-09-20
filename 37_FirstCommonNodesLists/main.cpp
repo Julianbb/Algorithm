8d24f81072bb10bd06ea164a79f713eea2d75cdd
julianbb <1546450025@qq.com>
2019-09-20 10:42:28 +0800

    second time

--- 37_FirstCommonNodesLists/main.cpp
+++ 37_FirstCommonNodesLists/main.cpp
@@ -3,19 +3,18 @@
 #include "../Utilities/List.h"
 using namespace std;
 
-
-int GetlengthOfList(const ListNode* pHead)
+int LengthOfList(ListNode* list)
 {
-    if(pHead == nullptr) return 0;
+    if(list == nullptr) return 0;
+    int count = 0;
 
-    int counter = 0;
-    const ListNode* tmp = pHead;
-    while(tmp != nullptr)
+    ListNode* node = list;
+    while(node != nullptr)
     {
-	counter++;
-	tmp = tmp->m_pNext;
+	node = node->m_pNext;
+	count++;
     }
-    return counter;
+    return count;
 }
 
 
@@ -23,39 +22,95 @@ int GetlengthOfList(const ListNode* pHead)
 ListNode* FindFirstCommonNode(ListNode* p1, ListNode* p2)
 {
     if(p1 == nullptr || p2 == nullptr) return nullptr;
-    
-    int L1 = GetlengthOfList(p1);
-    int L2 = GetlengthOfList(p2);
 
-    ListNode* tmpLong = p1;
-    ListNode* tmpShort = p2;
-    int gap = L1-L2;
-    if(L2>L1)
+    int l1 = LengthOfList(p1);
+    int l2 = LengthOfList(p2);
+    ListNode* node1 = p1;
+    ListNode* node2 = p2;
+
+
+    if(l1>l2)
     {
-        tmpLong = p2;
-        tmpShort = p1;
-	gap = L2-L1;
-    } 
-
-
-    while(gap--) // 先移动长的链表，至于连个链表相等
+	int diff1 = l1-l2;
+	for(int i=0; i<diff1; i++)
+	    node1 = node1->m_pNext;
+    }
+    else
     {
-	tmpLong = tmpLong->m_pNext;
+	int diff2 = l2-l1;
+	for(int i=0; i<diff2; i++)
+	    node2 = node2->m_pNext;
     }
 
 
-    while(tmpLong != tmpShort )
+    while(node1 != node2)
     {
-	tmpLong = tmpLong->m_pNext;
-	tmpShort = tmpShort->m_pNext;
+	node1 = node1->m_pNext;
+	node2 = node2->m_pNext;
     }
-   
-    if(tmpLong == tmpShort && tmpLong != nullptr)
-	return tmpLong;
+
+    if(node1 == node2 && node1 != nullptr)
+	return node1;
     else
 	return nullptr;
-
 }
+
+
+
+
+
+// int GetlengthOfList(const ListNode* pHead)
+// {
+//     if(pHead == nullptr) return 0;
+
+//     int counter = 0;
+//     const ListNode* tmp = pHead;
+//     while(tmp != nullptr)
+//     {
+//         counter++;
+//         tmp = tmp->m_pNext;
+//     }
+//     return counter;
+// }
+
+
+
+// ListNode* FindFirstCommonNode(ListNode* p1, ListNode* p2)
+// {
+//     if(p1 == nullptr || p2 == nullptr) return nullptr;
+
+//     int L1 = GetlengthOfList(p1);
+//     int L2 = GetlengthOfList(p2);
+
+//     ListNode* tmpLong = p1;
+//     ListNode* tmpShort = p2;
+//     int gap = L1-L2;
+//     if(L2>L1)
+//     {
+//         tmpLong = p2;
+//         tmpShort = p1;
+//         gap = L2-L1;
+//     }
+
+
+//     while(gap--) // 先移动长的链表，至于连个链表相等
+//     {
+//         tmpLong = tmpLong->m_pNext;
+//     }
+
+
+//     while(tmpLong != tmpShort )
+//     {
+//         tmpLong = tmpLong->m_pNext;
+//         tmpShort = tmpShort->m_pNext;
+//     }
+
+//     if(tmpLong == tmpShort && tmpLong != nullptr)
+//         return tmpLong;
+//     else
+//         return nullptr;
+
+// }
 
 
 
