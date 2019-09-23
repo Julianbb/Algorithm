c6b9d2a434712d1688d854a34813935aa458903a
julianbb <1546450025@qq.com>
2019-09-23 11:27:30 +0800

    second time

--- 45_LastNumberInCircle/main.cpp
+++ 45_LastNumberInCircle/main.cpp
@@ -11,22 +11,55 @@
 
 #include <iostream>
 #include <list>
+#include <vector>
 #include <stdio.h>
 #include "../Utilities/List.h"
 using namespace std;
 
 
-ListNode* CreatList(int n)
-{  
+int LastRemaining_Solution2(unsigned int n, unsigned int m)
+{
+    if(n<=0 || m<=0) return -1;
+    if(n==1)return n;
+    if(m==1)return n-1;
+
+    vector<int> array;
+    for(int i =0; i<n; i++)
+    {
+        array.push_back(i);
+    }
+
+    int pos = 1; 
+    vector<int>::iterator it = array.begin();
+    while(array.size() >1)
+    {
+        it++;
+        pos++;
+        if(it == array.end()) it= array.begin();
+
+        if(pos == m)
+        {
+            pos =1;
+            it = array.erase(it);
+
+            if(it == array.end()) it= array.begin();
+        }
+    }
+
+    return array[0];
+}
+
+ListNode* CreateList(int n)
+{
     ListNode* head = CreateListNode(0);
+
+
     ListNode* tmp = head;
-
-
     for(int i=1; i<n; i++)
     {
-        ListNode* current = CreateListNode(i);
-        tmp->m_pNext = current;
-        tmp = current;
+	ListNode* node = CreateListNode(i);
+	tmp->m_pNext = node;
+	tmp = node;
     }
 
     tmp->m_pNext = head;
@@ -34,40 +67,89 @@ ListNode* CreatList(int n)
 }
 
 
+int LastRemaining_Solution1(unsigned int n, unsigned int m)
+{
+    if(n<=0 || m<=0) return -1;
+    if(n==1)return n;
+    if(m==1)return n-1;
+
+    int count = 1;
+    ListNode* head = CreateList(n);
+
+    ListNode* tmp = head;
+    
+    while(tmp->m_pNext != tmp)
+    {
+
+    if(count == m-1)
+	{
+	    count = 1;
+	    ListNode* xx = tmp->m_pNext;
+	    tmp->m_pNext = xx->m_pNext;
+	    delete xx;
+	    tmp = tmp->m_pNext;
+        continue;
+	}
+
+	tmp = tmp->m_pNext;
+	count++;
+
+    }
+    return tmp->m_Value;
+}
+
+// ListNode* CreatList(int n)
+// {
+//     ListNode* head = CreateListNode(0);
+//     ListNode* tmp = head;
+
+
+//     for(int i=1; i<n; i++)
+//     {
+//         ListNode* current = CreateListNode(i);
+//         tmp->m_pNext = current;
+//         tmp = current;
+//     }
+
+//     tmp->m_pNext = head;
+//     return head;
+// }
+
+
 
 
 // construct list 
-int LastRemaining_Solution1(unsigned int n, unsigned int m)
-{
-    if(n == 1) return 0;
-    if(n <=0 ) return -1;
-    if(m <=0 ) return -1;
+// int LastRemaining_Solution1(unsigned int n, unsigned int m)
+// {
+//     if(n == 1) return 0;
+//     if(n <=0 ) return -1;
+//     if(m <=0 ) return -1;
 
-    int size = n;
+//     int size = n;
 
 
-    ListNode* pMy = CreatList(n);
-    
-    while(size>1)
-    {
-        int tmp = m-1;
-        while(--tmp)
-        {
-            pMy = pMy->m_pNext;
-        }
-        ListNode* pdelet = pMy->m_pNext;
-        
-        pMy->m_pNext = pMy->m_pNext->m_pNext;//删除结点
-        delete pdelet;
-        pdelet->m_pNext = nullptr;
+//     ListNode* pMy = CreatList(n);
 
-        size--;    
-        pMy = pMy->m_pNext; //定位到下次开始处  
-    }
+//     while(size>1)
+//     {
+//         int tmp = m-1;
+//         while(--tmp)
+//         {
+//             pMy = pMy->m_pNext;
+//         }
+//         ListNode* pdelet = pMy->m_pNext;
 
-    return pMy->m_Value;
+//         pMy->m_pNext = pMy->m_pNext->m_pNext;//删除结点
+//         delete pdelet;
+//         pdelet->m_pNext = nullptr;
 
-}
+//         size--;
+//         pMy = pMy->m_pNext; //定位到下次开始处
+//     }
+
+//     return pMy->m_Value;
+
+// }
 
 
 // take use of standard <list> lib
@@ -107,18 +189,18 @@ int LastRemaining_Solution1(unsigned int n, unsigned int m)
 // }
 
 
-int LastRemaining_Solution2(unsigned int n, unsigned int m)
-{
+// int LastRemaining_Solution2(unsigned int n, unsigned int m)
+// {
 
-    if(n < 1 || m < 1)
-    return -1;
+//     if(n < 1 || m < 1)
+//     return -1;
 
-    int last = 0;
-    for (int i = 2; i <= n; i ++)
-        last = (last + m) % i;
+//     int last = 0;
+//     for (int i = 2; i <= n; i ++)
+//         last = (last + m) % i;
 
-    return last;
-}
+//     return last;
+// }
 
 
 // ====================测试代码====================
